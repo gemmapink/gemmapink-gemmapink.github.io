c43f564c26d24cbe3cf3723f5097c87d669fb244
gemmapink <156139625+gemmapink@users.noreply.github.com>
2024-07-28 14:50:24 -0400

    if this doesn't work, go the the last commit

--- data.js
+++ data.js
@@ -1,146 +1,180 @@
-var APP_DATA = {
-  "scenes": [
-    {
-      "id": "0-museum",
-      "name": "museum",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Virtual Tour</title>
+    <style>
+        .hidden {
+            display: none;
         }
-      ],
-      "faceSize": 832,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.992487492905167,
-          "pitch": 0.28028489815937263,
-          "rotation": 0,
-          "target": "1-kay"
+    </style>
+</head>
+<body>
+    <div id="tour"></div>
+    
+    <script>
+        var APP_DATA = {
+            "scenes": [
+                {
+                    "id": "0-museum",
+                    "name": "museum",
+                    "levels": [
+                        {
+                            "tileSize": 256,
+                            "size": 256,
+                            "fallbackOnly": true
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 512
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 1024
+                        }
+                    ],
+                    "faceSize": 832,
+                    "initialViewParameters": {
+                        "pitch": 0,
+                        "yaw": 0,
+                        "fov": 1.5707963267948966
+                    },
+                    "linkHotspots": [
+                        {
+                            "yaw": 2.992487492905167,
+                            "pitch": 0.28028489815937263,
+                            "rotation": 0,
+                            "target": "1-kay"
+                        }
+                    ],
+                    "infoHotspots": [
+                        {
+                            "yaw": 2.069047906666503,
+                            "pitch": -1,
+                            "title": "museum",
+                            "text": "Welcome to the historic residential district of West Bel Air Avenue! This virtual tour was begun by a volunteer intern at the Aberdeen Historical Museum. It features information and pictures of various houses and other structures. The tour aims to inform you about the history of these buildings and the people who lived and worked in them. Please note that the tour is still a work in progress, and more buildings and details will be added over time. However, the  information provided in this tour is just a small percentage of the history of West Bel Air Avenue and greater Aberdeen. If you would like to contribute to the tour by providing feedback, reporting bugs or glitches, or sharing additional knowledge about these places on West Bel Air Avenue, please fill out this form. To learn more about Aberdeen, please visit our website. We hope you enjoy the tour!"
+                        }
+                    ]
+                },
+                {
+                    "id": "1-kay",
+                    "name": "kay",
+                    "levels": [
+                        {
+                            "tileSize": 256,
+                            "size": 256,
+                            "fallbackOnly": true
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 512
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 1024
+                        }
+                    ],
+                    "faceSize": 1024,
+                    "initialViewParameters": {
+                        "pitch": 0,
+                        "yaw": 0,
+                        "fov": 1.5707963267948966
+                    },
+                    "linkHotspots": [
+                        {
+                            "yaw": -3.0096431302777447,
+                            "pitch": 0.117909292881194,
+                            "rotation": 0,
+                            "target": "2-orion"
+                        },
+                        {
+                            "yaw": -0.32555316640704035,
+                            "pitch": 0.8362599263516728,
+                            "rotation": 0,
+                            "target": "0-museum"
+                        }
+                    ],
+                    "infoHotspots": [
+                        {
+                            "yaw": -1.464036955665911,
+                            "pitch": 0.17639538874157346,
+                            "title": "kay house",
+                            "text": "ring ring"
+                        }
+                    ]
+                },
+                {
+                    "id": "2-orion",
+                    "name": "orion",
+                    "levels": [
+                        {
+                            "tileSize": 256,
+                            "size": 256,
+                            "fallbackOnly": true
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 512
+                        },
+                        {
+                            "tileSize": 512,
+                            "size": 1024
+                        }
+                    ],
+                    "faceSize": 1024,
+                    "initialViewParameters": {
+                        "pitch": 0,
+                        "yaw": 0,
+                        "fov": 1.5707963267948966
+                    },
+                    "linkHotspots": [
+                        {
+                            "yaw": 2.8911952755398875,
+                            "pitch": 0.22148641978154515,
+                            "rotation": 0,
+                            "target": "0-museum"
+                        },
+                        {
+                            "yaw": 1.9774598637924186,
+                            "pitch": 0.10561048357118707,
+                            "rotation": 0,
+                            "target": "1-kay"
+                        }
+                    ],
+                    "infoHotspots": [
+                        {
+                            "yaw": 1.436059191979795,
+                            "pitch": -0.13198257214868647,
+                            "title": "orion",
+                            "text": ""
+                        }
+                    ]
+                }
+            ],
+            "name": "Project Title",
+            "settings": {
+                "mouseViewMode": "drag",
+                "autorotateEnabled": true,
+                "fullscreenButton": false,
+                "viewControlButtons": false
+            }
+        };
+
+        function initializeTour(data) {
+            data.scenes.forEach(scene => {
+                scene.infoHotspots = scene.infoHotspots.filter(hotspot => hotspot.text.trim() !== '');
+            });
+
+            console.log(data); // For demonstration purposes, log the modified data
+            // Here you would initialize your virtual tour using the modified data.
+            // Example: initVirtualTour(data);
         }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 2.069047906666503,
-          "pitch": -1,
-          "title": "museum",
-          "text": "Welcome to the historic residential district of West Bel Air Avenue! This virtual tour was begun by a volunteer intern at the Aberdeen Historical Museum. It features information and pictures of various houses and other structures. The tour aims to inform you about the history of these buildings and the people who lived and worked in them. Please note that the tour is still a work in progress, and more buildings and details will be added over time. However, the  information provided in this tour is just a small percentage of the history of West Bel Air Avenue and greater Aberdeen. If you would like to contribute to the tour by providing feedback, reporting bugs or glitches, or sharing additional knowledge about these places on West Bel Air Avenue, please fill out this form. To learn more about Aberdeen, please visit our website. We hope you enjoy the tour!"
-        }
-      ]
-    },
-    {
-      "id": "1-kay",
-      "name": "kay",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -3.0096431302777447,
-          "pitch": 0.117909292881194,
-          "rotation": 0,
-          "target": "2-orion"
-        },
-        {
-          "yaw": -0.32555316640704035,
-          "pitch": 0.8362599263516728,
-          "rotation": 0,
-          "target": "0-museum"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": -1.464036955665911,
-          "pitch": 0.17639538874157346,
-          "title": "kay house",
-          "text": "ring ring"
-        }
-      ]
-    },
-    {
-      "id": "2-orion",
-      "name": "orion",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        }
-      ],
-      "faceSize": 1024,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 2.8911952755398875,
-          "pitch": 0.22148641978154515,
-          "rotation": 0,
-          "target": "0-museum"
-        },
-        {
-          "yaw": 1.9774598637924186,
-          "pitch": 0.10561048357118707,
-          "rotation": 0,
-          "target": "1-kay"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 1.436059191979795,
-          "pitch": -0.13198257214868647,
-          "title": "orion",
-          "text": "coal"
-        }
-      ]
-    }
-  ],
-  "name": "Project Title",
-  "settings": {
-    "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
-  }
-};
+
+        document.addEventListener('DOMContentLoaded', function() {
+            initializeTour(APP_DATA);
+        });
+    </script>
+</body>
+</html>
+
